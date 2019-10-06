47639579e9fe39ead756d5bc089359da49c7ca8d
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-10-06 21:25:33 +0100

    basic logic + assembly

--- src/main/java/faph/calendarvalidator/CalendarValidator.java
+++ src/main/java/faph/calendarvalidator/CalendarValidator.java
@@ -5,6 +5,14 @@ package faph.calendarvalidator;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
 
 public class CalendarValidator {
 
@@ -17,10 +25,20 @@ public class CalendarValidator {
                 .addObject(calendarValidator)
                 .build()
                 .parse(argv);
-        calendarValidator.run();
+        try {
+            calendarValidator.run();
+        } catch (IOException ex) {
+            Logger.getLogger(CalendarValidator.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserException ex) {
+            Logger.getLogger(CalendarValidator.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    public void run() {
+    public void run() throws FileNotFoundException, IOException, ParserException {
         System.out.printf("Running validation on file %s", filepath);
+        FileInputStream inputStream = new FileInputStream(filepath);
+        CalendarBuilder builder = new CalendarBuilder();
+        Calendar calendar = builder.build(inputStream);
+        calendar.validate(true);
     }
 }
